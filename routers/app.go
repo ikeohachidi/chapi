c729aaa0a2a3faa834fbbd21d5429af0bd201111
ikeohachidi <ikeohachidi@gmail.com>
2021-08-21 14:40:05 +0100

    feat: write errRedirect helper

--- routers/app.go
+++ routers/app.go
@@ -1,16 +1,28 @@
 package routers
 
 import (
-	"github.com/ikeohachidi/chapi-be/models"
+	"net/http"
+	"os"
+
+	goSession "github.com/gorilla/sessions"
+	"github.com/ikeohachidi/chapi-be/model"
 	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
 )
+
+var store = goSession.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
 type App struct {
 	echo.Context
-	Db models.Conn
+	Db model.Conn
 }
 
 type Response struct {
 	Data       interface{} `json:"message"`
 	Successful bool        `json:"successful"`
+}
+
+func errRedirect(c echo.Context, url string, err error) {
+	log.Error(err)
+	c.Redirect(http.StatusInternalServerError, url)
 }
