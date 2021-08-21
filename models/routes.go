8b6cb1426acba7b44e212378cb83439fcb7f89f8
ikeohachidi <ikeohachidi@gmail.com>
2021-08-21 13:27:04 +0100

    feat: update route table queries

--- models/routes.go
+++ models/routes.go
@@ -4,16 +4,21 @@ import "time"
 
 type Route struct {
 	Id          uint      `json:"id" db:"id"`
-	Path        string    `json:"path" db:"path"`
 	ProjectId   uint      `json:"project_id" db:"project_id"`
-	RequestBody string    `json:"request_body" db:"request_body"`
+	Type        string    `json:"type" db:"type"`
+	Path        string    `json:"path" db:"path"`
+	Destination string    `json:"destination" db:"destination"`
+	Body        string    `json:"body" db:"body"`
 	CreatedAt   time.Time `json:"created_at" db:"created_at"`
 }
 
-func (c *Conn) CreateRoute(route Route) (routeId uint, err error) {
+func (c *Conn) SetRoute(route Route) (routeId uint, err error) {
 	stmt, err := c.db.Preparex(`
-		INSERT INTO routes (path, project_id, request_body, created_at)
-		VALUES ($1, $2, $3, $4)
+		INSERT INTO routes (id, project_id, type, path, destination, body)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		ON CONFLICT (id)
+		DO UPDATE SET 
+		type = EXCLUDED.type, path = EXCLUDED.path, destination = EXCLUDED.destination, body = EXCLUDED.body
 		RETURNING id
 	`)
 	if err != nil {
@@ -34,7 +39,7 @@ func (c *Conn) CreateRoute(route Route) (routeId uint, err error) {
 }
 
 func (c *Conn) DeleteRoute(routeId uint) (err error) {
-	_, err = c.db.Exec("DELETE FROM routes WHERE id=$1")
+	_, err = c.db.Exec("DELETE FROM routes WHERE id=$1", routeId)
 
 	if err != nil {
 		return
