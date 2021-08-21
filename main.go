f7b209b17def5add315328d0de26ee513d06658b
ikeohachidi <ikeohachidi@gmail.com>
2021-08-21 13:26:36 +0100

    chore: remove unused code

--- main.go
+++ main.go
@@ -9,21 +9,17 @@ import (
 func main() {
 	e := echo.New()
 	db := models.Connect()
-	lama := "hello world"
 
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			cc := routers.App{
 				c,
 				db,
-				lama,
 			}
 
 			return next(cc)
 		}
 	})
-
-	// e.GET("/project", routers.HandleHome)
 
 	e.Logger.Fatal(e.Start(":1333"))
 }
