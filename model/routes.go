d4aca4aba55230808556ed8ba864ddc38958b6c6
ikeohachidi <ikeohachidi@gmail.com>
2021-08-27 01:26:14 +0100

    feat: add sql query to get routes from name

--- model/routes.go
+++ model/routes.go
@@ -1,17 +1,21 @@
 package model
 
-import "time"
+import (
+	"database/sql"
+	"log"
+	"time"
+)
 
 type Route struct {
-	ID          uint      `json:"id" db:"id"`
-	ProjectID   uint      `json:"projectId" db:"project_id"`
-	UserID      uint      `json:"userId" db:"user_id"`
-	Type        string    `json:"type" db:"type"`
-	Path        string    `json:"path" db:"path"`
-	Destination string    `json:"destination" db:"destination"`
-	Body        string    `json:"body" db:"body"`
-	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
-	Queries     []Query   `json:"queries" db:"queries"`
+	ID          uint           `json:"id" db:"id"`
+	ProjectID   uint           `json:"projectId" db:"project_id"`
+	UserID      uint           `json:"userId" db:"user_id, -"`
+	Type        string         `json:"type" db:"type"`
+	Path        string         `json:"path" db:"path"`
+	Destination string         `json:"destination" db:"destination"`
+	Body        sql.NullString `json:"body" db:"body"`
+	CreatedAt   time.Time      `json:"createdAt" db:"created_at"`
+	Queries     []Query        `json:"queries" db:"queries"`
 }
 
 // SaveRoute will either create a new Route or update and existing one
@@ -65,6 +69,21 @@ func (c *Conn) GetRoutesByProjectId(projectID uint) (routes []Route, err error) 
 
 func (c *Conn) DeleteRoute(routeID uint, userID uint) (err error) {
 	_, err = c.db.Exec("DELETE FROM routes WHERE id=$1 AND user_id=$2", routeID, userID)
+
+	return
+}
+
+func (c *Conn) GetRouteFromNameAndPath(name string, path string) (route Route, err error) {
+	queryStmt := `
+		SELECT * FROM route
+		WHERE project_id = (
+				SELECT id FROM project WHERE "name" = $1 
+			)
+		AND path = $2 
+	`
+	log.Printf("over here %v %v", name, path)
+
+	err = c.db.Get(&route, queryStmt, name, path)
 
 	return
 }
