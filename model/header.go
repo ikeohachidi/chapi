bf0e2a587d5aa2bba14633380d845e7b3f1ce6d9
ikeohachidi <ikeohachidi@gmail.com>
2021-10-11 21:44:47 +0100

    feat: update Header struct

--- model/header.go
+++ model/header.go
@@ -1,23 +1,18 @@
 package model
 
 type Header struct {
-	ID      uint   `json:"id,omitempty" db:"id"`
-	UserID  uint   `json:"user_id,omitempty" db:"user_id"`
-	RouteID uint   `json:"route_id,omitempty" db:"route_id"`
-	Name    string `json:"name" db:"name"`
-	Value   string `json:"value" value:"value"`
+	ID    uint   `json:"id,omitempty" db:"id"`
+	Name  string `json:"name" db:"name"`
+	Value string `json:"value" value:"value"`
 }
 
 func (c *Conn) SaveHeader(header Header, userID, routeID uint) (err error) {
 	stmt := `
 		INSERT INTO	header(user_id, route_id, name, value)
 		VALUES ($1, $2, $3, $4)
-		ON CONFLICT (name)
-		DO UPDATE
-		SET value = EXCLUDED.value
 	`
 
-	_, err = c.db.Exec(stmt, header.UserID, header.RouteID, header.Name, header.Value)
+	_, err = c.db.Exec(stmt, userID, routeID, header.Name, header.Value)
 	if err != nil {
 		return
 	}
@@ -28,14 +23,14 @@ func (c *Conn) SaveHeader(header Header, userID, routeID uint) (err error) {
 func (c *Conn) DeleteHeader(headerName string, userID, routeID uint) (err error) {
 	stmt, err := c.db.Prepare(`
 		DELETE FROM header
-		WHERE name = $1 AND user_id = $2 AND route_id = $3
+		WHERE "name" = $1 AND user_id = $2 AND route_id = $3
 	`)
 
 	if err != nil {
 		return
 	}
 
-	_, err = stmt.Exec(stmt, headerName, userID, routeID)
+	_, err = stmt.Exec(headerName, userID, routeID)
 	if err != nil {
 		return
 	}
