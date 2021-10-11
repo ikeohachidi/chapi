299ca25b4b2c586a3314806e0a1634357f60646e
ikeohachidi <ikeohachidi@gmail.com>
2021-10-11 20:24:38 +0100

    feat: add dummy data for header

--- model/schema.go
+++ model/schema.go
@@ -41,12 +41,21 @@ CREATE TABLE IF NOT EXISTS "query" (
 	value 			VARCHAR(50) NOT NULL
 );
 
+DROP TABLE IF EXISTS header CASCADE;
+CREATE TABLE IF NOT EXISTS header (
+	user_id 		INTEGER REFERENCES "user"(id) ON DELETE CASCADE,
+	route_id	 	INTEGER REFERENCES route(id) ON DELETE CASCADE,
+	name 			VARCHAR(40) NOT NULL,
+	value 			VARCHAR(40) NOT NULL
+);
+
 -- dummy data
 INSERT into "user"(email) values('ikeohachidi@gmail.com');
 INSERT INTO project("name", user_id) VALUES('foo', 1);
 INSERT INTO project("name", user_id) VALUES('bar', 1);
 INSERT into route(project_id, user_id, method, path, destination) VALUES(1, 1, 'GET', '/maps', 'http://localhost.com');
 INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, 'key1', 'private1');
+INSERT INTO header(user_id, route_id, "name", "value") VALUES(1, 1, 'Authorization', 'just-some-random-key');
 INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, 'key2', 'private2');
 INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, 'key3', 'private3');
 `
