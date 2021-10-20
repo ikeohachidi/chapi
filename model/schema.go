fb7ea3caf7c8fb0da44bc254082c25ac78bb0dae
ikeohachidi <ikeohachidi@gmail.com>
2021-10-20 21:00:12 +0100

    feat: update schema defintion

--- model/schema.go
+++ model/schema.go
@@ -1,8 +1,12 @@
 package model
 
-var schema = `
+import "fmt"
+
+var schema = fmt.Sprintf(`
 DROP TYPE IF EXISTS request_type CASCADE; 
 CREATE TYPE request_type AS ENUM ('GET', 'POST', 'PUT', 'DELETE');
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 DROP TABLE IF EXISTS "user" CASCADE;
 CREATE TABLE "user" (
@@ -37,8 +41,8 @@ CREATE TABLE IF NOT EXISTS "query" (
 	id 				SERIAL PRIMARY KEY,
 	route_id		INTEGER REFERENCES route(id) ON DELETE CASCADE,
 	user_id 		INTEGER REFERENCES "user"(id) ON DELETE CASCADE,
-	name 			VARCHAR(20) NOT NULL,
-	value 			VARCHAR(50) NOT NULL
+	name 			TEXT NOT NULL,
+	value 			TEXT NOT NULL
 );
 
 DROP TABLE IF EXISTS header CASCADE;
@@ -46,8 +50,8 @@ CREATE TABLE IF NOT EXISTS header (
 	id 				SERIAL PRIMARY KEY,
 	user_id 		INTEGER REFERENCES "user"(id) ON DELETE CASCADE,
 	route_id	 	INTEGER REFERENCES route(id) ON DELETE CASCADE,
-	name 			VARCHAR(40) NOT NULL,
-	value 			VARCHAR(40) NOT NULL
+	name 			TEXT NOT NULL,
+	value 			TEXT NOT NULL
 );
 
 --- array function
@@ -65,10 +69,7 @@ INSERT INTO project("name", user_id) VALUES('bar', 1);
 
 INSERT into route(project_id, user_id, method, path, destination) VALUES(1, 1, 'GET', '/maps', 'http://localhost:5001');
 
-INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, 'key1', 'private1');
-INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, 'key2', 'private2');
-INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, 'key3', 'private3');
+INSERT INTO "query"(route_id, user_id, "name", "value") VALUES(1, 1, pgp_sym_encrypt('key1', '%v'), pgp_sym_encrypt('private1', '%v'));
 
-INSERT INTO header(user_id, route_id, "name", "value") VALUES(1, 1, 'Authorization', 'just-some-random-key');
-INSERT INTO header(user_id, route_id, "name", "value") VALUES(1, 1, 'Bearer-Token', '-random-key');
-`
+INSERT INTO header(user_id, route_id, "name", "value") VALUES(1, 1, pgp_sym_encrypt('Authorization', '%v'), pgp_sym_encrypt('-random-key', '%v'));
+`, PG_CRYPT_KEY, PG_CRYPT_KEY, PG_CRYPT_KEY, PG_CRYPT_KEY)
