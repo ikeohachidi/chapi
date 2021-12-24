0fc40d8efa0ed9552bfe520951a95c05dea43ba9
ikeohachidi <ikeohachidi@gmail.com>
2021-12-24 13:51:53 +0100

    feat: install toast component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Toast from "vue-toastification";
+
 import 'remixicon/fonts/remixicon.css';
+import "vue-toastification/dist/index.css";
 
 import { Route, NavigationGuardNext } from 'vue-router';
 
 import './index.css';
 
 Vue.config.productionTip = false
+Vue.use(Toast);
 
 router.beforeEach(async (to: Route, from: Route, next: NavigationGuardNext) => {
   if (to.meta!.requiresAuth) {
@@ -28,4 +32,5 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
+.$mount('#app')
