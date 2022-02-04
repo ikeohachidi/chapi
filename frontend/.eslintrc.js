21e86e76731f511307074c3b9481803322538ac9
ikeohachidi <ikeohachidi@gmail.com>
2022-02-04 20:41:53 +0100

    chore: update eslint rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,8 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off'
   },
   overrides: [
     {
